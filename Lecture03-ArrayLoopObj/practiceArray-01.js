b5e2b79c469c0a9588b92d295b76e3744189e8f1
thanteeeee <dandt270948@gmail.com>
2024-12-25 10:36:05 +0700

    practice

---
+++ Lecture03-ArrayLoopObj/practiceArray-01.js
@@ -0,0 +1,13 @@
+let values1 = ['Apple',1,false] ;
+let values2 = ['Frise',2,true] ;
+let values3 = ['Mars',9,'Apple'] ;
+
+let newvalues = [] ;
+
+values1.forEach((value,index) => {
+
+    if(values1.value === values2.value){
+
+    }
+    
+});
