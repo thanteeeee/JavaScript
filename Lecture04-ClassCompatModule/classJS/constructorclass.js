09e578d103364c3a8968e03e99a3fe05687a87b1
thanteeeee <dandt270948@gmail.com>
2024-12-25 08:41:00 +0700

    update

---
+++ Lecture04-ClassCompatModule/classJS/constructorclass.js
@@ -0,0 +1,9 @@
+class song {
+    constructor(title,artist){
+        this.title = title ;
+        this.artist = artist ;
+    }
+}
+
+const mysong = new song("Bohemian Rhapsody", "Queen") ;
+console.log(mysong.title);
