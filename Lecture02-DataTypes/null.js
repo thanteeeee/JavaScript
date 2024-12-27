46ab791c58540f72b692a7a64e1cdddbd7ef8a4a
thanteeeee <dandt270948@gmail.com>
2024-12-27 17:39:08 +0700

    update

--- Lecture02-DataTypes/null.js
+++ Lecture02-DataTypes/null.js
@@ -1,5 +1,7 @@
 function getVowels(str) {
-    const m = str.match(/[aeiou]/gi);
+    const m = str.match(/[aeiou]/gi); 
+    //g: หมายถึง global ซึ่งจะค้นหาทุกตำแหน่งในข้อความ (ไม่หยุดที่เจอครั้งแรก)
+    //i: หมายถึง case-insensitive ซึ่งไม่สนใจตัวพิมพ์เล็กหรือใหญ่ (จับได้ทั้ง A และ a)
     console.log(m);
     if (m === null) {
         return 0 ;
