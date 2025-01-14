bd9b6f5e14684c821dc7807f0fe15526be2a453f
thanteeeee <dandt270948@gmail.com>
2025-01-15 00:35:18 +0700

    update

--- Lecture05-Asychronous/asynccatch.js
+++ Lecture05-Asychronous/asynccatch.js
@@ -2,7 +2,7 @@ function yay0rNay() {
     return new Promise((resolve, reject) => {
         const val = Math.round(Math.random() * 1);
         // 0 or 1, at random
-        val ? resolve("Lucky!!") : reject("Nope ");
+        val ? resolve("Lucky!!") : reject("Nope 😠");
     })
 }
 
@@ -18,7 +18,7 @@ async function msg() {
 msg(); //Lucky!!
 msg(); //Lucky!!
 msg(); //Lucky!!
-msg(); //Nope
+msg(); //Nope😠
 msg(); //Lucky!!
-msg(); //Nope
-msg(); //Nope
+msg(); //Nope😠
+msg(); //Nope😠
--- Lecture05-Asychronous/moreasyncawait.js
+++ Lecture05-Asychronous/moreasyncawait.js
@@ -1,7 +1,7 @@
 function who() {
     return new Promise((resolve) => {
         setTimeout(() => {
-            resolve("");
+            resolve("🤡");
         }, 200);
     });
 }
@@ -30,5 +30,5 @@ async function msg() {
     console.log(`${a} ${b} ${c}`);
 }
 console.log('We are looking for : ');
-msg(); //  lurks in the shadows <-- after 1 second
+msg(); // 🤡lurks in the shadows <-- after 1 second
 console.log('Hello');
